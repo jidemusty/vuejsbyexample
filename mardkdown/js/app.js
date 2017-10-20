8dd4ea9a6ea6f95c7efb6d6e119076ae5ee0a299
jidemusty <jidemustapha2013@gmail.com>
2017-10-20 15:01:47 +0100

    simple markdown

---
+++ mardkdown/js/app.js
@@ -0,0 +1,36 @@
+let Preview = {
+    props: ['text'],
+    template: `
+        <div class="editor__preview" v-html="markdownText"></div>
+    `,
+    computed: {
+        markdownText () {
+            return marked(this.text, { sanitize: true })
+        }
+    }
+}
+
+let Editor = {
+    components: {
+        'preview': Preview
+    },
+    data () {
+        return {
+            text: ''
+        }
+    },
+    template: `
+        <div class="editor">
+            <textarea v-model="text" cols="30" rows="10" class="editor__input"></textarea>
+            <preview :text="text"></preview>
+        </div>
+    `
+}
+
+let app = new Vue({
+    el: '#app',
+    components: {
+        'editor': Editor
+    }
+})
+        
