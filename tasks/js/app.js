29a24207a02dd8ce4aef9d64604a40badd45d87c
jidemusty <jidemustapha2013@gmail.com>
2017-10-20 13:02:51 +0100

    can add task

--- tasks/js/app.js
+++ tasks/js/app.js
@@ -19,6 +19,35 @@ let Task = {
     }
 }
 
+let TaskForm = {
+    data () {
+        return {
+            body: null
+        }
+    },
+    template: `
+        <form action="#" @submit.prevent="addTask">
+            <input type="text" v-model.trim="body" />
+            <button>add task</button>
+        </form>
+    `,
+    methods: {
+        addTask () {
+            if (!this.body) {
+                return
+            }
+
+            bus.$emit('task:added', {
+                id: Date.now(),
+                body: this.body,
+                done: false
+            })
+
+            this.body = null
+        }
+    }
+}
+
 let Tasks = {
     data () {
         return {
@@ -30,16 +59,18 @@ let Tasks = {
     },
     template: `
         <div>
-            <template v-if="tasks.length">
-                <div class="tasks">
+            <div class="tasks">
+                <template v-if="tasks.length">
                     <task v-for="task in tasks" :key="task.id" :task="task"></task>
-                </div>
-            </template>
-            <span v-else>No Task</span>
+                </template>
+                <span v-else>No Task</span>
+            </div>
+            <task-form></task-form>
         </div>
     `,
     components: {
-        'task': Task
+        'task': Task,
+        'task-form': TaskForm
     },
     methods: {
         toggleDone (taskId) {
@@ -64,6 +95,10 @@ let Tasks = {
 
         bus.$on('task:delete', (taskId) => {
             this.deleteTask(taskId)
+        });
+
+        bus.$on('task:added', (task) => {
+            this.tasks.unshift(task);
         });
     }
 }
