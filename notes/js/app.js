4e1bfb02559aa0dfdbc8ca46f0b62da1da6d1c6d
jidemusty <jidemustapha2013@gmail.com>
2017-11-03 22:10:44 +0100

    styling

---
+++ notes/js/app.js
@@ -0,0 +1,16 @@
+let Notes = {
+    template: `
+        <div class="notes">
+            <a class="notes__new" href="#">
+                Create a new note
+            </a>
+        </div>
+    `
+}
+
+let app = new Vue({
+    el: "#app",
+    components: {
+
+    }
+})
