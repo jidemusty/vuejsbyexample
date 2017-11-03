7d437e10a2e9753019ec605f7f7fe941c80fdbad
jidemusty <jidemustapha2013@gmail.com>
2017-11-03 22:21:55 +0100

    create a new note

--- notes/js/app.js
+++ notes/js/app.js
@@ -1,16 +1,31 @@
 let Notes = {
+    data () {
+        return {
+            notes: []
+        }
+    },
     template: `
         <div class="notes">
-            <a class="notes__new" href="#">
+            <a class="notes__new" href="#" @click.prevent="addNote">
                 Create a new note
             </a>
         </div>
-    `
+    `,
+    methods: {
+        addNote () {
+            this.notes.unshift({
+                id: Date.now(),
+                body: ''
+            })
+
+            localStorage.setItem('notes', JSON.stringify(this.notes));
+        }
+    }
 }
 
 let app = new Vue({
     el: "#app",
     components: {
-
+        'notes': Notes
     }
 })
