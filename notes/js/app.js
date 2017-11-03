7185180b657b9659d541f0833b87239a83d42a6c
jidemusty <jidemustapha2013@gmail.com>
2017-11-03 22:55:24 +0100

    outputting notes

--- notes/js/app.js
+++ notes/js/app.js
@@ -1,7 +1,34 @@
-let Notes = {
+let Note = {
+    props: [
+        'noteObject'
+    ],
     data () {
         return {
-            notes: []
+            open: false,
+            note: this.noteObject
+        }
+    },
+    template: `
+        <div class="note__wrapper">
+            <div class="note__header">
+                <a href=# class="note">
+                    <span>{{ note.body || 'Empty Note' }}</span>
+                    <span>5 words</span>
+                </a>
+                <a v-if="open" href="#" class="note__delete">Delete note</a>
+            </div>
+            <textarea v-if="open" class="editor" rows="10" placeholder="Write a note"></textarea>
+        </div>
+    `
+}
+
+let Notes = {
+    components: {
+        'note': Note
+    },
+    data () {
+        return {
+            notes: JSON.parse(localStorage.getItem('notes')) || []
         }
     },
     template: `
@@ -9,6 +36,7 @@ let Notes = {
             <a class="notes__new" href="#" @click.prevent="addNote">
                 Create a new note
             </a>
+            <note v-for="note in notes" :note-object="note" :key="note.id"></note>
         </div>
     `,
     methods: {
